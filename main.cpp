af6e2ab0d5375f1bcf71cc44c18656cadfde4b90
pfife00 <forest.pfeiffer@gmail.com>
2020-04-07 11:48:06 -0700

    Add CarColor class

--- main.cpp
+++ main.cpp
@@ -4,10 +4,36 @@ using std::cout;
 using std::endl;
 using std::string;
 
+class CarColor
+{
+public:
+
+  // Set default car color to blue
+  CarColor ();
+
+  CarColor (string color);
+
+private:
+  string color;
+};
+
+CarColor::CarColor()
+{
+  color = blue;
+}
+
+CarColor::CarColor (string color)
+{
+  this->color = color;
+
+}
 // Simple hello World program
 int main()
 {
-  string hello = "Hello World";
+
+  CarColor carColor;
+
+  string hello = "Hello";
 
   cout << hello << endl;
 
