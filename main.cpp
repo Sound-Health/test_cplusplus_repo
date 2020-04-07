a7719f9deb9a882f37804f3b4ba41eaecdee4cd0
pfife00 <forest.pfeiffer@gmail.com>
2020-04-07 11:56:36 -0700

    ADD COMMENTSgit add .git add .git add .git add .git add .git add .git add .git add .git add .git add .

--- main.cpp
+++ main.cpp
@@ -11,32 +11,40 @@ public:
   // Set default car color to blue
   CarColor ();
 
+  // Overloaded constructor
   CarColor (string color);
 
+// ****************** Getters ****************** //
   string getColor ();
 
+// ****************** Setters ****************** //
   void setColor (string color);
 
 private:
+
   string color;
 };
 
+// Default color is blue
 CarColor::CarColor()
 {
   color = blue;
 }
 
+// Can initialize color to some other color
 CarColor::CarColor (string color)
 {
   this->color = color;
 
 }
 
+// Returns the color
 string CarColor::getColor ()
 {
   return color;
 }
 
+// Sets the color
 void CarColor::setColor (string color)
 {
   this->color = color;
@@ -46,15 +54,20 @@ void CarColor::setColor (string color)
 int main()
 {
 
+  // Initialize string color
   string color;
+
+  // Create instance of CarColor class
   CarColor carColor;
 
+  // Get user input for color
   string hello = "Hello what is your favorite color car?";
-
   cin >> color;
 
+  // Set the color
   carColor.setColor(color);
 
+  // Print out the color
   cout << carColor.getColor() << endl;
 
   return 0;
