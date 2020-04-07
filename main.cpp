9cf571195ef8e6882fd46746fc65e545f9a9b939
pfife00 <forest.pfeiffer@gmail.com>
2020-04-07 10:44:59 -0700

    Included main.cpp and makefile

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+// Simple hello World program
+int int main() {
+
+  string hello = "Hello World";
+
+  cout << hello << endl;
+  
+  return 0;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+build:
+	g++ -std=c++11 src/*.cpp -o c++_code_test
